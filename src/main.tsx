d190e7ec168797b235ddaa4559ffd3f54a530723
Rakshit <rakshit087@gmail.com>
2025-04-01 09:57:12 +0530

    feat: add React Query for data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,25 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import './index.css';
 import App from './App.tsx';
 import Dashboard from './pages/dashboard.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<App />}
-        />
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<App />}
+          />
+          <Route
+            path="/dashboard"
+            element={<Dashboard />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
